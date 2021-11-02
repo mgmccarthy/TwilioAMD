2a5618e1b34aa02f95fbca72b86f2fba4f690e4c
Mike McCarthy <mike_mccarthy3@yahoo.com>
2021-11-02 12:22:35 -0400

    Add project files.

---
+++ TwilioAMD.API/Controllers/CallbackController.cs
@@ -0,0 +1,59 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Primitives;
+using Twilio.AspNet.Core;
+using Twilio.TwiML;
+
+namespace TwilioAMD.API.Controllers
+{
+    [ApiController]
+    [Route("callback")]
+    public class CallbackController : TwilioController
+    {
+        [HttpPost]
+        [Route("index")]
+        public IActionResult Index()
+        {
+            var response = new VoiceResponse();
+
+            if (Request.Form.TryGetValue("AnsweredBy", out var answeredBy))
+            {
+                //async amd tutorial
+                //https://www.twilio.com/blog/async-answering-machine-detection-tutorial
+
+                Debug.WriteLine($"answeredBy: {answeredBy}");
+
+                //SyncAmd(answeredBy, response);
+                AsyncAmd(answeredBy, response);
+            }
+
+            return Content(response.ToString(), "text/xml"); 
+        }
+
+        private static void AsyncAmd(StringValues answeredBy, VoiceResponse response)
+        {
+            //when a human answers, this is never invoked...
+            if (answeredBy == "human")
+            {
+                response.Say("this is the message I'm going to ask the callee and gather input");
+            }
+            else //for AMD async callback URL, AnsweredBy is always "unknown", and no message is left on my voicemail
+            {
+                response.Say("this is the voice message");
+            }
+        }
+
+        private static void SyncAmd(StringValues answeredBy, VoiceResponse response)
+        {
+            //when using non-async option with regular callback URL
+            if (answeredBy == "machine_end_beep" || answeredBy == "machine_end_silence" || answeredBy == "machine_end_other")
+            {
+                response.Say("this is the voice message");
+            }
+            else
+            {
+                response.Say("this is the message I'm going to ask the callee and gather input");
+            }
+        }
+    }
+}
---
+++ TwilioAMD/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Twilio;
+using Twilio.Http;
+using Twilio.Rest.Api.V2010.Account;
+
+namespace TwilioAMD
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var configuration = new ConfigurationBuilder().SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+                .AddJsonFile("appsettings.json", true)
+                .AddUserSecrets<Program>()
+                .Build();
+
+            var accountSid = configuration["accountSid"];
+            var authToken = configuration["authToken"];
+
+            TwilioClient.Init(accountSid, authToken);
+
+            var call = CallResource.Create(
+                machineDetection: "DetectMessageEnd",
+                
+                //asyncAMD settings
+                asyncAmd: "true",
+                asyncAmdStatusCallback: new Uri("http://bdb0-2601-40a-8002-2730-d900-c49a-9f5d-5499.ngrok.io/callback/index"),
+                asyncAmdStatusCallbackMethod: HttpMethod.Post,
+                
+                //these two are for TwimL callback... we're not using a callback to get Twiml, we're sending it
+                //url: new Uri("http://bdb0-2601-40a-8002-2730-d900-c49a-9f5d-5499.ngrok.io/callback/index"),
+                //method: HttpMethod.Post,
+                
+                twiml: new Twilio.Types.Twiml("<Response><Say>Ahoy there!</Say></Response>"),
+                from: new Twilio.Types.PhoneNumber(configuration["fromPhoneNumber"]),
+                to: new Twilio.Types.PhoneNumber(configuration["toPhoneNumber"])
+            );
+
+            Console.WriteLine(call.Sid);
+        }
+    }
+}
