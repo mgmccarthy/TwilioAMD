ed4a580676def220fd70b99a191749dbc705784a
Mike McCarthy <mike_mccarthy3@yahoo.com>
2021-11-11 09:04:48 -0500

    Uncomented the Twiml for initial prompt that is very much like the prompt we'll be reading to the recipient in the system.
    - remove WeatherForecaseController

--- TwilioAMD.API/Controllers/CallbackController.cs
+++ TwilioAMD.API/Controllers/CallbackController.cs
@@ -42,10 +42,10 @@ namespace TwilioAMD.API.Controllers
                 //machineDetectionSpeechEndThreshold: 5000,
                 //machineDetectionSilenceTimeout: 10000,
 
-                twiml: new Twilio.Types.Twiml("<Response><Say>Praesent in mauris eu tortor porttitor accumsan. Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Aenean fermentum risus id tortor. Integer imperdiet lectus quis justo. Integer tempor. Vivamus ac urna vel leo pretium faucibus. Mauris elementum mauris vitae tortor. In dapibus augue non sapien. Aliquam ante. Curabitur bibendum justo non orci. Morbi leo mi, nonummy eget, tristique non, rhoncus non, leo. Nullam faucibus mi quis velit. Integer in sapien. Fusce tellus odio, dapibus id, fermentum quis, suscipit id, erat. Fusce aliquam vestibulum ipsum. Aliquam erat volutpat. Pellentesque sapien. Cras elementum. Nulla pulvinar eleifend sem. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque porta. Vivamus porttitor turpis ac leo.Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nullam feugiat, turpis at pulvinar vulputate, erat libero tristique tellus, nec bibendum odio risus sit amet ante. Aliquam erat volutpat. Nunc auctor. Mauris pretium quam et urna. Fusce nibh. Duis risus. Curabitur sagittis hendrerit ante. Aliquam erat volutpat. Vestibulum erat nulla, ullamcorper nec, rutrum non, nonummy ac, erat. Duis condimentum augue id magna semper rutrum. Nullam justo enim, consectetuer nec, ullamcorper ac, vestibulum in, elit. Proin pede metus, vulputate nec</Say></Response>"),
+                //twiml: new Twilio.Types.Twiml("<Response><Say>Praesent in mauris eu tortor porttitor accumsan. Mauris suscipit, ligula sit amet pharetra semper, nibh ante cursus purus, vel sagittis velit mauris vel metus. Aenean fermentum risus id tortor. Integer imperdiet lectus quis justo. Integer tempor. Vivamus ac urna vel leo pretium faucibus. Mauris elementum mauris vitae tortor. In dapibus augue non sapien. Aliquam ante. Curabitur bibendum justo non orci. Morbi leo mi, nonummy eget, tristique non, rhoncus non, leo. Nullam faucibus mi quis velit. Integer in sapien. Fusce tellus odio, dapibus id, fermentum quis, suscipit id, erat. Fusce aliquam vestibulum ipsum. Aliquam erat volutpat. Pellentesque sapien. Cras elementum. Nulla pulvinar eleifend sem. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Quisque porta. Vivamus porttitor turpis ac leo.Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Nullam feugiat, turpis at pulvinar vulputate, erat libero tristique tellus, nec bibendum odio risus sit amet ante. Aliquam erat volutpat. Nunc auctor. Mauris pretium quam et urna. Fusce nibh. Duis risus. Curabitur sagittis hendrerit ante. Aliquam erat volutpat. Vestibulum erat nulla, ullamcorper nec, rutrum non, nonummy ac, erat. Duis condimentum augue id magna semper rutrum. Nullam justo enim, consectetuer nec, ullamcorper ac, vestibulum in, elit. Proin pede metus, vulputate nec</Say></Response>"),
                 
                 //with this Twiml, I'm getting call not in progress in asyncAMD callback
-                //twiml: new Twilio.Types.Twiml("<Response><Say>This is McCarthy Primary Care calling to remind you of your upcoming appointment on November 13th, 2021 at ten o'clock A.M. with Dr. McCarthy. If you need to cancel your appointment, please contact us during normal business hours at 867-5309. Thanks!</Say></Response>"),
+                twiml: new Twilio.Types.Twiml("<Response><Say>This is McCarthy Primary Care calling to remind you of your upcoming appointment on November 13th, 2021 at ten o'clock A.M. with Dr. McCarthy. If you need to cancel your appointment, please contact us during normal business hours at 867-5309. Thanks!</Say></Response>"),
                 
                 from: new Twilio.Types.PhoneNumber(configuration["fromPhoneNumber"]),
                 to: new Twilio.Types.PhoneNumber(configuration["toPhoneNumber"])
@@ -69,11 +69,14 @@ namespace TwilioAMD.API.Controllers
                 Debug.WriteLine($"callSid: {callSid}");
                 Debug.WriteLine($"accountSid: {accountSid}");
 
-                //for me, this is "machine_end_beep"
+                //answeredBy is "machine_end_beep", when I send the call to voicemail on my mobile phone or I let it ring out and it falls to voicemail
+                //but when I answer the phone and don't say antyhing, this callback is invoked about half way through the initial prompt, and answeredBy is "unknown", which then interuppts
+                //the current initial prompt and reads this one... which I don't want to do.
+                //HOWEVER, when I answer the phone and say "hello", answeredBy is "human"
                 if (answeredBy != "human")
                 {
                     var call = CallResource.Update(
-                        twiml: new Twilio.Types.Twiml("<Response><Say>This is the voicemail I'm going to leave for you instead of asking you a question b/c you're talking to a machine.</Say></Response>"),
+                        twiml: new Twilio.Types.Twiml("<Response><Say>This is the voicemail I'm going to leave for you instead of asking you a question because you're talking to a machine.</Say></Response>"),
                         pathSid: callSid,
                         pathAccountSid: accountSid
                     );
